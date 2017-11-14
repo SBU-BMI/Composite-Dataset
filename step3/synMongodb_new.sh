44b5f7940bc4a9e1e446e5aebb693fe0bc550038
bridge wang <tigerfan7495@gmail.com>
2017-11-14 14:34:02 -0500

    Create synMongodb_new.sh

---
+++ step3/synMongodb_new.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input_file="case_id_list_2017_11_03"
+
+IFS=$'\n' read -d '' -r -a caseid_list < $input_file
+
+for case_id in "${caseid_list[@]}"
+do  
+  qsub -v caseid=$case_id synMongodb_new.pbs 
+  #echo $case_id
+  #break 
+done
