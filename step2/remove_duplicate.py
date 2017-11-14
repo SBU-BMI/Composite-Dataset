a1251e794da5d021d039337095dbcb0e9aa54090
bridge wang <tigerfan7495@gmail.com>
2017-11-14 14:49:36 -0500

    Create remove_duplicate.py

---
+++ step2/remove_duplicate.py
@@ -0,0 +1,9 @@
+db.metadata.aggregate(
+    {
+	$match : {"provenance.analysis_execution_id":{'$regex' : 'composite_dataset', '$options' : 'i'}}
+    },
+    {
+	$group : { _id : "$image.case_id", total : { $sum : 1 } }
+    }
+  );
+  
