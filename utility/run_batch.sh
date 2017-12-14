bdf03ed55221b8ddfa0c1083f93e8b327bfa6ce3
bridge wang <tigerfan7495@gmail.com>
2017-12-14 15:50:53 -0500

    Create run_batch.sh

---
+++ utility/run_batch.sh
@@ -0,0 +1,11 @@
+# run all pbs files
+qsub composite_dataset01.pbs
+qsub composite_dataset02.pbs
+qsub composite_dataset03.pbs
+qsub composite_dataset04.pbs
+qsub composite_dataset05.pbs
+qsub composite_dataset06.pbs
+qsub composite_dataset07.pbs
+qsub composite_dataset08.pbs
+qsub composite_dataset09.pbs
+qsub composite_dataset10.pbs
